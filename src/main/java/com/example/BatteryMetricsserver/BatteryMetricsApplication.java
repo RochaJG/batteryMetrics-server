0018072409658687bed573a37d6240839603f6ce
Jordan Rocha <70rd23@gmail.com>
2020-01-13 16:14:45 -0300

    Init API project

---
+++ src/main/java/com/example/BatteryMetricsserver/BatteryMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.example.BatteryMetricsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatteryMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatteryMetricsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BatteryMetricsserver/BatteryMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BatteryMetricsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatteryMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
